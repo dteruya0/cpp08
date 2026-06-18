c51fa394b55e2fba57afbc90cc70fe97ac694aaf
Diego Teruya <dteruya@c2r7p5.42sp.org.br>
2026-06-18 19:30:52 -0300

    finished

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,12 +6,32 @@
 /*   By: dteruya <dteruya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/17 14:56:28 by dteruya           #+#    #+#             */
-/*   Updated: 2026/03/17 14:57:28 by dteruya          ###   ########.fr       */
+/*   Updated: 2026/06/18 19:28:53 by dteruya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+    public:
+        typedef typename std::stack<T>::container_type::iterator iterator;
+
+        iterator begin()
+        {
+            return this->c.begin();
+        }
+
+        iterator end()
+        {
+            return this->c.end();
+        }
+};
 
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,73 @@
 /*   By: dteruya <dteruya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/17 14:52:34 by dteruya           #+#    #+#             */
-/*   Updated: 2026/03/17 14:52:35 by dteruya          ###   ########.fr       */
+/*   Updated: 2026/06/18 19:28:32 by dteruya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "MutantStack.hpp"
+
+
+#include <iostream>
+#include <list>
+#include "MutantStack.hpp"
+
+int main()
+{
+    std::cout << "===== TESTE MUTANTSTACK =====" << std::endl;
+
+    MutantStack<int> mstack;
+
+    mstack.push(5);
+    mstack.push(17);
+
+    std::cout << "top: " << mstack.top() << std::endl;
+
+    mstack.pop();
+
+    std::cout << "size depois do pop: " << mstack.size() << std::endl;
+
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    mstack.push(0);
+
+    std::cout << "\nPercorrendo com iterator:" << std::endl;
+
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+
+    std::cout << "\n===== TESTE COPIA PARA STD::STACK =====" << std::endl;
+
+    std::stack<int> s(mstack);
+
+    std::cout << "top da std::stack copiada: " << s.top() << std::endl;
+    std::cout << "size da std::stack copiada: " << s.size() << std::endl;
+
+    std::cout << "\n===== TESTE COM STD::LIST =====" << std::endl;
+
+    std::list<int> lista;
+
+    lista.push_back(5);
+    lista.push_back(3);
+    lista.push_back(5);
+    lista.push_back(737);
+    lista.push_back(0);
+
+    std::list<int>::iterator lit = lista.begin();
+    std::list<int>::iterator lite = lista.end();
+
+    while (lit != lite)
+    {
+        std::cout << *lit << std::endl;
+        ++lit;
+    }
+
+    return 0;
+}
